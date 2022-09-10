8013244040970969b0b52f04867aa2770428d94c
Omer Ahmed <omera407@gmail.com>
2022-09-10 16:44:05 -0600

    Most basic functionality! It reads my twitch chat and spits out everything it receives into the console.

--- TwitchChatGameUnity/Assets/Scripts/TwitchChat.cs
+++ TwitchChatGameUnity/Assets/Scripts/TwitchChat.cs
@@ -15,13 +15,55 @@ public class TwitchChat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(TwitchAccountCredentials.username);
+        //Debug.Log(TwitchAccountCredentials.username);
         //Debug.Log(TwitchAccountCredentials.password); Probably shouldn't keep this line in, for obvious reasons.
-        Debug.Log(TwitchAccountCredentials.channelName);
+        //Debug.Log(TwitchAccountCredentials.channelName);
+
+        reconnectAfter = 60.0f;
+        Connect();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (twitchClient.Available == 0)
+        {
+            reconnectTimer += Time.deltaTime;
+
+            if(reconnectTimer >= reconnectAfter)
+            {
+                Connect();
+                reconnectTimer = 0f;
+            }
+        }
+
+        //Debug.Log(twitchClient.Available);
+        //Debug.Log(twitchClient.GetStream());
+        ReadChat();
+    }
+
+    private void Connect()
+    {
+        twitchClient = new TcpClient("irc.chat.twitch.tv", 6667);
+        reader = new StreamReader(twitchClient.GetStream());
+        writer = new StreamWriter(twitchClient.GetStream());
+        writer.WriteLine("PASS " + TwitchAccountCredentials.password);
+        writer.WriteLine("NICK " + TwitchAccountCredentials.username);
+        writer.WriteLine("USER " + TwitchAccountCredentials.username + " 8 *:" + TwitchAccountCredentials.username);
+        writer.WriteLine("JOIN #" + TwitchAccountCredentials.channelName);
+        writer.Flush();
+    }
+
+    public void ReadChat()
+    {
+        if(twitchClient.Available > 0)
+        {
+            string message = reader.ReadLine();
+
+            if (message.Contains("PRIVMSG"))
+            {
+                Debug.Log(message);
+            }
+        }
     }
 }
